ec8bdfa2ce9c918071a363758a1ae2e74b48a3c2
Troeglazova <otroeglazova@primeaspect.io>
2021-03-25 17:48:39 +0600

    в классе User в equals добавлено поле List<Data>

--- src/main/java/io/primeaspect/csvparser/model/User.java
+++ src/main/java/io/primeaspect/csvparser/model/User.java
@@ -54,11 +54,11 @@ public class User {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return id == user.id && Objects.equals(name, user.name);
+        return id == user.id && Objects.equals(name, user.name) && Objects.equals(data, user.data);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name);
+        return Objects.hash(id, name, data);
     }
 }
